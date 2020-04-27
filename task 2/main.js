1371ee2c12d8f28cac41c3697d1658d8785e482b
vchepurnaya <v.chepurnayaw@gmail.com>
2020-04-27 20:34:36 +0300

    add comments

--- task 2/main.js
+++ task 2/main.js
@@ -1,13 +1,13 @@
 var array = ['apple', 'orange', 'grape', 'orange'];
-/*array.unshift('pineapple');
-array.push('banana');
-delete array [0];
-array.splice(4,1);*/
+array.unshift('pineapple'); //1
+array.push('banana'); //2
+delete array [0]; //3
+array.splice(4,1);
 
-for (var i=0; i<array.length; i++) {
+/*for (var i=0; i<array.length; i++) {
   var newArray = [];
   if (array[i]===array[i]) {
     delete array[i];
   }
-}
+}*/
 console.log(array);
