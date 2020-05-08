0ee5e0bbf9dd4d2d6b14b37a5e932d9d02818506
vchepurnaya <v.chepurnayaw@gmail.com>
2020-05-09 00:04:54 +0300

    update js & css

--- task 3/js/main.js
+++ task 3/js/main.js
@@ -3,6 +3,24 @@ var elBody = document.body;
 var btnToggle = document.querySelector('._toggle');
 var arrOfSpans = document.querySelectorAll('.l-tasks_span');
 var btnRemove = document.querySelector('._remove');
+var clickToggleHandler = function() {
+  elBody.classList.toggle('_color-scheme-light');
+};
+var removeSpanHandler = function() {
+  var lastSpan = document.querySelectorAll('.l-tasks_span')[arrOfSpans.length-1];
+  var firstSpan = document.querySelectorAll('.l-tasks_span')[0];
+
+  if (elBody.classList.contains('_color-scheme-vivid')) {
+    lastSpan.remove();
+  }
+  else {
+    firstSpan.remove();
+  }
+  btnRemove.removeEventListener('click', removeSpanHandler);
+};
+
+btnToggle.addEventListener('click', clickToggleHandler);
+btnRemove.addEventListener('click', removeSpanHandler);
 
 ;(function(num) {
    if (typeof num === 'number' ){
@@ -18,26 +36,7 @@ var btnRemove = document.querySelector('._remove');
    }
 }(5));
 
-var clickToggleHandler = function() {
-  elBody.classList.toggle('_color-scheme-light');
-};
 
-btnToggle.addEventListener('click', clickToggleHandler);
 
-var removeSpanHandler = function() {
-  var lastSpan = document.querySelectorAll('.l-tasks_span')[arrOfSpans.length-1];
-  var firstSpan = document.querySelectorAll('.l-tasks_span')[0];
-  var counter = 0;
 
-    if (elBody.classList.contains('_color-scheme-vivid')) {
-      lastSpan.remove();
-    }
-    else {
-      firstSpan.remove();
-      if (counter > 1){
-      }
-      btnRemove.removeEventListener('click', removeSpanHandler);
-    }
-};
 
-btnRemove.addEventListener('click', removeSpanHandler);
