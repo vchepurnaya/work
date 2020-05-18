0a20aabf6b9c26fd81f7557e6f14055fc5db05b1
vchepurnaya <v.chepurnayaw@gmail.com>
2020-05-18 23:37:32 +0300

    add code in js task 4

--- task 4/js/main.js
+++ task 4/js/main.js
@@ -2,13 +2,25 @@ var galaxy = document.querySelector('.b-universe');
 var earth = document.querySelector('.b-universe__img');
 
 galaxy.addEventListener('click', function (e) {
+var x = e.offsetX - (earth.offsetWidth / 2);
+var y = e.offsetY - (earth.offsetHeight / 2);
 
-earth.style.transform = 'translate(' + e.offsetX + 'px, ' +  e.offsetY + 'px)';
-
-  if (earth === e.target ) {
-    earth.style.display = 'none';
-  } else {
-    earth.style.display = '';
+if (x < 0) {
+ x = 0;
+} else if (e.offsetX + (earth.offsetWidth/2) > galaxy.offsetWidth) {
+  x = galaxy.offsetWidth - earth.offsetWidth;
+}
+  if (y < 0) {
+    y = 0;
+  } else if (e.offsetY + (earth.offsetHeight/2) > galaxy.offsetHeight) {
+    y = galaxy.offsetHeight - earth.offsetHeight;
   }
 
+  earth.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+
+ if (e.target.tagName === 'IMG') {
+   earth.style.display = 'none';
+ } else if (earth.style.display === 'none') {
+   earth.style.display = '';
+  }
 });
