853c84c9e05748fccfde9a5f4e97539d16aafc16
vchepurnaya <v.chepurnayaw@gmail.com>
2020-05-19 00:03:42 +0300

    update js task4

--- task 4/js/main.js
+++ task 4/js/main.js
@@ -5,22 +5,23 @@ galaxy.addEventListener('click', function (e) {
 var x = e.offsetX - (earth.offsetWidth / 2);
 var y = e.offsetY - (earth.offsetHeight / 2);
 
+  if (e.target.tagName.toLowerCase() === 'img') {
+    return earth.style.display = 'none';
+  }
+  if (earth.style.display === 'none') {
+    return earth.style.display = '';
+  }
+
 if (x < 0) {
  x = 0;
-} else if (e.offsetX + (earth.offsetWidth/2) > galaxy.offsetWidth) {
+} else if (e.offsetX + (earth.offsetWidth / 2) > galaxy.offsetWidth) {
   x = galaxy.offsetWidth - earth.offsetWidth;
 }
   if (y < 0) {
     y = 0;
-  } else if (e.offsetY + (earth.offsetHeight/2) > galaxy.offsetHeight) {
+  } else if (e.offsetY + (earth.offsetHeight / 2) > galaxy.offsetHeight) {
     y = galaxy.offsetHeight - earth.offsetHeight;
   }
 
   earth.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
-
- if (e.target.tagName === 'IMG') {
-   earth.style.display = 'none';
- } else if (earth.style.display === 'none') {
-   earth.style.display = '';
-  }
 });
