33cba6f385f9734748d4659772980a0ba0bbb060
Vika Chepurnaya <v.chepurnayaw@gmail.com>
2020-06-16 00:46:46 +0300

    add 3 part without last point

--- task 7 (game)/js/main.js
+++ task 7 (game)/js/main.js
@@ -2,6 +2,8 @@ var graveyard = document.querySelector('.b-shooter');
 var shooter = document.querySelector('.b-shooter__aim');
 var imgOfShooter = document.querySelector('.b-shooter__img-aim');
 var ghost = document.querySelector('.b-shooter__img-ghost');
+var fire = document.querySelector('.b-shooter__img-fire');
+var intervalIdGhost = setInterval(setIntervalForGhost,3000);
 
 graveyard.addEventListener('click', function (e) {
   var x = e.offsetX - shooter.offsetWidth / 2;
@@ -30,16 +32,39 @@ document.addEventListener('keydown', function (e) {
 });
 
 document.addEventListener('keyup', function (e) {
-  var rect = shooter.getBoundingClientRect();
-  var aimCenterX = rect.x;
-  var aimCenterY = rect.y;
+  var rectGhost = ghost.getBoundingClientRect();
+  var aimCenterX = shooter.getBoundingClientRect().x + shooter.getBoundingClientRect().width / 2;
+  var aimCenterY = shooter.getBoundingClientRect().y + shooter.getBoundingClientRect().height / 2;
+  var delayToReset = 500;
 
   if (e.code === 'Enter') {
     imgOfShooter.style.transform = 'scale(1,1)';
-  }
 
-  console.log(aimCenterX, aimCenterY);
+    if (aimCenterY >= rectGhost.top + 20 &&
+        aimCenterY <= rectGhost.bottom - 20 &&
+        aimCenterX >= rectGhost.left + 20 && 
+        aimCenterX <= rectGhost.right - 20) {
+
+      setAnimation();
+      clearInterval(intervalIdGhost);
+      setTimeout(resetAnimation, delayToReset);
+    }
+
+    console.log(aimCenterY, aimCenterX);
+  }
 });
+
+function setAnimation() {
+  fire.classList.toggle('animation');
+  ghost.classList.toggle('animation');
+  imgOfShooter.style.display = 'none';
+};
+
+function resetAnimation() {
+  fire.classList.toggle('animation');
+  ghost.classList.toggle('animation');
+  imgOfShooter.style.display = '';
+};
 
 function setRandomCoords() {
   var x = Math.floor(Math.random() * (graveyard.offsetWidth - ghost.offsetWidth));
@@ -47,26 +72,15 @@ function setRandomCoords() {
   var limitX = graveyard.offsetWidth - ghost.offsetWidth;
   var limitY = graveyard.offsetHeight - shooter.offsetHeight;
 
-  if (x < 0) {
-    x = 0;
-  } else if (x > limitX) {
-    x = limitX;
-  }
-  if (y < 0) {
-    y = 0;
-  } else if (y > limitY) {
-    y = limitY;
-  }
-
   ghost.style.left =  x + 'px';
   ghost.style.top =  y + 'px';
 };
 
-setInterval(function () {
-if (ghost.style.display === 'none') {
-  ghost.removeAttribute('style');
-} else if (ghost.style.display === '') {
-  ghost.setAttribute('style', 'display: none;');
-}
-setRandomCoords();
-}, 3000);
+function setIntervalForGhost() {
+  if (ghost.style.display === 'none') {
+    ghost.removeAttribute('style');
+  } else if (ghost.style.display === '') {
+    ghost.setAttribute('style', 'display: none;');
+  }
+  setRandomCoords();
+};
