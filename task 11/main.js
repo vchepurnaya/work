ed9506875606b72d165b05a6ea786bd2730507fb
Vika Chepurnaya <v.chepurnayaw@gmail.com>
2020-06-20 00:34:37 +0300

    add task 11

---
+++ task 11/main.js
@@ -0,0 +1,35 @@
+const requestForUsers = function(success, error, url){
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', url);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status >=200 && xhr.status < 300) {
+                success(xhr.response);
+                console.log(JSON.parse(xhr.response));
+            } else {
+                error({
+                    code: xhr.status,
+                    message: xhr.responseText
+                });
+            }
+        }
+    }
+
+xhr.send();
+}
+
+requestForUsers(male => {
+    console.log(male);
+}, err => {
+            console.log(err);
+        }, 'https://randomuser.me/api/?results=3&gender=male'
+);
+
+requestForUsers(female => {
+    console.log(female);
+}, err => {
+            console.log(err);
+        }, 'https://randomuser.me/api/?results=3&gender=female'
+);
